ed0fc26c399d7a73c285eb51e7898b27af25eb08
William Hogan <hoganwr@gmail.com>
2016-10-30 11:49:53 -0400

    close to working version beta of dron command line DL query tool

---
+++ src/main/java/edu/ufl/bmi/ontology/dronquery/DlQueryExecutor.java
@@ -0,0 +1,70 @@
+package edu.ufl.bmi.ontology.dronquery;
+
+import static org.semanticweb.owlapi.search.EntitySearcher.getAnnotationObjects;
+
+import java.util.Set;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.expression.OWLEntityChecker;
+import org.semanticweb.owlapi.expression.ShortFormEntityChecker;
+import org.semanticweb.owlapi.manchestersyntax.parser.ManchesterOWLSyntaxClassExpressionParser;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.NodeSet;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProvider;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProviderAdapter;
+import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+
+import edu.ufl.bmi.ontology.dronquery.AnnotationExtractor;
+
+
+public final class DlQueryExecutor {
+
+    private final OWLReasonerFactory reasonerFactory;
+    private final OWLOntology ontology;
+
+    private final IRI labelIri = IRI.create("http://www.w3.org/2000/01/rdf-schema#label");
+
+    private OWLReasoner reasoner;
+
+    public DlQueryExecutor(OWLReasonerFactory reasonerFactory, OWLOntology inputOntology) {
+        this.reasonerFactory = reasonerFactory;
+        ontology = inputOntology;
+    }
+
+    private String labelFor(OWLClass clazz) {
+        /*
+         * Use a visitor to extract label annotations
+         */
+        AnnotationExtractor le = new AnnotationExtractor(labelIri);
+        for (OWLAnnotation anno : getAnnotationObjects(clazz, ontology)) {
+            anno.accept(le);
+        }
+        /* Print out the label if there is one. If not, just use the class URI */
+        if (le.getResult() != null) {
+            return le.getResult();
+        } else {
+            return clazz.getIRI().toString();
+        }
+    }
+
+
+    public NodeSet<OWLClass> runQuery(String dlQueryTxt) {
+	if (reasoner == null) {
+	    reasoner = reasonerFactory.createNonBufferingReasoner(ontology);
+	}
+
+	OWLOntologyManager manager = ontology.getOWLOntologyManager();
+        ShortFormProvider sfp = new SimpleShortFormProvider();
+        Set<OWLOntology> importClosure = ontology.getImportsClosure();
+        BidirectionalShortFormProvider bidiShortFormProvider = new BidirectionalShortFormProviderAdapter(manager, importClosure, sfp);
+        
+        ManchesterOWLSyntaxClassExpressionParser parser = new ManchesterOWLSyntaxClassExpressionParser(manager.getOWLDataFactory(), new ShortFormEntityChecker(bidiShortFormProvider));
+        OWLClassExpression clExp = parser.parse(dlQueryTxt);
+       
+        NodeSet<OWLClass> result = reasoner.getSubClasses(clExp, false);        
+        return result;
+    }
+}
