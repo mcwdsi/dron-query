b94031cb08827900fd803966427f67c9c348d049
William R. Hogan <hoganwr@gmail.com>
2018-12-07 13:00:40 -0500

    added a script to run the 'drug product' AND 'packaged drug product' query, which checks whether there was an error where one IRI got assigned to two classes, one a descendant of 'drug product' and one a descendant of 'packaged drug product'.  Nothing should be both.

--- dron-qa-scripts/run-dron-qa.sh
+++ dron-qa-scripts/run-dron-qa.sh
@@ -1,8 +1,10 @@
+./dron-qa-scripts/run-dp-vs-pdp.sh "$1" $2
 ./dron-qa-scripts/run-chem-entity.sh "$1" $2
 ./dron-qa-scripts/run-processed-material.sh "$1" $2
 ./dron-qa-scripts/run-drug-product.sh "$1" $2
 ./dron-qa-scripts/run-packaged-drug-product.sh "$1" $2
 
+cat dp-vs-pdp-results-$2.txt | wc -l
 cat chem-entity-results-$2.txt | wc -l
 cat processed-material-results-$2.txt | wc -l
 cat drug-product-results-$2.txt | wc -l
